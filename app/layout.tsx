8ebeb62d76546038bf9e8e5fc56ff09082507746
Ari Zerner <ari@zerner.com>
2025-01-16 15:06:18 -0800

    Enable Vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import Footer from "./components/Footer";
 import { APP_NAME } from "./lib/constants";
+import { Analytics } from "@vercel/analytics/react"
 import "./globals.css";
 
 const geistSans = Geist({
